bd8973e0ccba145d6be69befe292d0c2352798d1
MistaAsh <ashishbharath2002@gmail.com>
2022-04-11 03:45:26 +0530

    Basic html, django and model

---
+++ unchained/articles/models.py
@@ -0,0 +1,12 @@
+from email.quoprimime import body_check
+from django.db import models
+
+# Create your models here.
+
+class Article(models.Model):
+    title = models.CharField(max_length = 200)
+    slug = models.SlugField() 
+    body = models.TextField()
+    date = models.DateTimeField(auto_now_add = True)
+    # Add in thumbnail later
+    # Add in author later
